843829f65375bfba04240681000d9f2cb1701c54
zeroy <zeroy0410@gmail.com>
2024-11-16 22:16:14 +0800

    up

--- cmd/taintanalysis/case/main.go
+++ cmd/taintanalysis/case/main.go
@@ -2,20 +2,48 @@ package main
 
 import "fmt"
 
-func processValue(v interface{}) {
-	switch v.(type) {
-	case string:
-		fmt.Println("String value:", v)
-	case int:
-		fmt.Println("Integer value:", v)
-	default:
-		fmt.Println("Unknown type")
-	}
+type Person interface {
+	Run()
+}
+
+type Stu struct {
+	Name string
+	Age  int
+}
+
+func (a *Stu) Run() {
+	fmt.Println(a.Name, "is running")
+}
+
+type Tea struct {
+	Name string
+	Age  int
+}
+
+func (t *Tea) Run() {
+	fmt.Println(t.Name, "is running")
 }
 
 func main() {
-	var i interface{} = true
+	// 创建 Stu 和 Tea 的实例
+	stu1 := &Stu{Name: "Alice", Age: 20}
+	tea1 := &Stu{Name: "Bob", Age: 30}
 
-	// 我们没有处理 bool 类型的情况
-	processValue(i)
+	// 创建一个 Person 类型的切片，存储 Stu 和 Tea 的实例
+	persons := []Person{stu1, tea1}
+
+	// 遍历切片并调用 Run 方法
+	for _, person := range persons {
+		person.Run()
+	}
+
+	// 进一步测试类型断言
+	for _, person := range persons {
+		switch p := person.(type) {
+		case *Stu:
+			fmt.Println("This is a student:", p.Name)
+		case *Tea:
+			fmt.Println("This is a teacher:", p.Name)
+		}
+	}
 }
